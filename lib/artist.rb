b7711b01be92c793f4a1a84f448214e95bba8c81
Nadia Lee <miss.nadia.lee@gmail.com>
2018-01-23 01:08:41 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,8 @@ class Artist
     @@song_count += 1
   end
 
-  def add_song_by_name(name, artist)
-    song = Song.new(name, artist)
+  def add_song_by_name(name)
+    song = Song.new(name, self)
     @songs << song
     song.artist = self
     @@song_count += 1
